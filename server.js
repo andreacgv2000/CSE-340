88e643b9bbdc5b678da1db56e4dd31da7062b41c
andreacgv2000 <andreacgv2000@gmail.com>
2026-01-17 18:27:44 -0600

    w01

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
 const express = require("express")
-const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+// Serve static files from public folder
+app.use(express.static("public"))
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = 3000
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`Servidor escuchando en http://localhost:${port}`)
 })
+
+
+
+
